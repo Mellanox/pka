8e2a5c12b2ce3e2cdba7027e8223ec88467d89f9
shihyic <103535873+shihyic@users.noreply.github.com>
2022-12-28 23:42:44 -0500

    Update to support BF3

--- engine/helper/pka_helper.h
+++ engine/helper/pka_helper.h
@@ -63,6 +63,8 @@ extern "C" {
 
 #define PKA_ENGINE_QUEUE_CNT        8
 #define PKA_ENGINE_RING_CNT         4
+#define PKA_ENGINE_QUEUE_CNT_BF3   24
+#define PKA_ENGINE_RING_CNT_BF3     4
 
 #define PKA_ENGINE_INSTANCE_NAME    "SSL engine"
 
